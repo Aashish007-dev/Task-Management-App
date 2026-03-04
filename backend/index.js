4d624ac923de9217225ce021e120de2a277e4987
Aashish Chotaliya <aashishchotaliya2003@gmail.com>
2026-03-04 20:54:06 +0530

    Initialize backend project with Express, MongoDB connection, and basic server setup.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,24 @@
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import connectDB from './config/db.js';
 
-const app = express();
+const app = express()
 
+await connectDB();
+
+app.use(express.json());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    
+}));
 
 
 
 
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+    console.log('Server is running on port 3000!');
 })
