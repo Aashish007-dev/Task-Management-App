188a37b9fb8297f4aab69a44e638e228d478aa54
Aashish Chotaliya <aashishchotaliya2003@gmail.com>
2026-03-11 11:17:57 +0530

    Implement Mongoose user model and task management API with create and get operations.

--- backend/controller/task.controller.js
+++ backend/controller/task.controller.js
@@ -1,6 +1,7 @@
 import { errorHandler } from "../utils/error.js";
 import TaskModel from "../models/task.model.js";
 
+
 export const createTaskController = async (req, res, next) => {
     try {
         const { title, description, priority, dueDate, assignedTo, attachments, todoCheckList } = req.body;
@@ -25,6 +26,74 @@ export const createTaskController = async (req, res, next) => {
             message: "Task created successfully",
             task
         })
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+export const getTasksController = async (req, res, next) => {
+    try {
+        const {status} = req.query;
+
+        let filter = {};
+
+        if(status){
+            filter.status = status
+        }
+
+        let tasks
+
+        if(req.user.role === "admin"){
+            tasks = await TaskModel.find(filter).populate("assignedTo", "name email profileImageUrl")
+
+        }else{
+            tasks = await TaskModel.find({
+                ...filter,
+                assignedTo: req.user.id
+            }).populate("assignedTo", "name email profileImageUrl")
+        }
+
+        tasks = await Promise.all(tasks.map(async (task) => {
+            const completedCount = task.todoCheckList.filter((item) => item.completed).length;
+
+            return {...task._doc, completedCount: completedCount}
+
+        }))
+
+        // status summary count
+
+        const allTasks = await TaskModel.countDocuments(
+            req.user.role === "admin" ? {} : {assignedTo: req.user.id}
+        )
+        const pendingTasks = await TaskModel.countDocuments({
+            ...filter,
+            status: "Pending",
+            ...(req.user.role !== "admin" && {assignedTo: req.user.id})
+        })
+        const inProgressTasks = await TaskModel.countDocuments({
+            ...filter,
+            status: "In Progress",
+            ...(req.user.role !== "admin" && {assignedTo: req.user.id})
+        })
+        const completedTasks = await TaskModel.countDocuments({
+            ...filter,
+            status: "Completed",
+            ...(req.user.role !== "admin" && {assignedTo: req.user.id})
+        })
+
+        res.status(200).json({
+            success: true,
+            message: "Tasks fetched successfully",
+            tasks,
+            statusSummary: {
+                all: allTasks,
+                pendingTasks,
+                inProgressTasks,
+                completedTasks
+            }
+        })
+
     } catch (error) {
         next(error)
     }
--- backend/routes/task.route.js
+++ backend/routes/task.route.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { adminOnly, verifyToken } from "../utils/verifyUser.js";
-import { createTaskController } from "../controller/task.controller.js";
+import { createTaskController, getTasksController } from "../controller/task.controller.js";
 
 const router = express.Router();
 
 
 router.post('/create', verifyToken, adminOnly, createTaskController)
+
+router.get('/', verifyToken, getTasksController)
 
 
 
