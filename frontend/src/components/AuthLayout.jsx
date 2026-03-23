2c7fa45427e16b3a3a9e83a2ade91a7a225a89bf
Aashish Chotaliya <aashishchotaliya2003@gmail.com>
2026-03-23 17:43:51 +0530

    Login Ul build

---
+++ frontend/src/components/AuthLayout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AuthLayout = ({children}) => {
+  return (
+    <div className='flex h-screen overflow-hidden'>
+        <div className='w-full md:w-1/2 overflow-y-auto my-auto'>
+          <div className='min-h-full flex flex-col px-12 pt-8 pb-12 '>
+            <div className="flex-grow flex items-center justify-center">
+                {children}
+            </div>
+          </div>
+        </div>
+
+        <div className="hidden md:block w-1/2">
+          <img src="/src/assets/login_img.jpg" alt="login-background" className='h-full w-full object-cover' />
+        </div>
+    </div>
+  )
+}
+
+export default AuthLayout
