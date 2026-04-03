ded37c9bf7560849ce6f0fc86c5033f2b78e5540
Aashish Chotaliya <aashishchotaliya2003@gmail.com>
2026-04-03 11:14:54 +0530

    integrate all users api

--- frontend/src/pages/admin/ManageUsers.jsx
+++ frontend/src/pages/admin/ManageUsers.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosInstance from '../../utils/axiosInstance';
+import DashboardLayout from '../../components/DashboardLayout';
 
 const ManageUsers = () => {
+
+  const [allUsers, setAllUsers] = useState([]);
+
+  const getAllUsers = async () => {
+    try {
+      const response = await axiosInstance.get('/users/get-users');
+      if(response.data?.length > 0){
+        setAllUsers(response.data)
+      }
+    } catch (error) {
+      console.log("Error fetching Users:", error);
+    }
+  }
+
+  useEffect(() => {
+    getAllUsers();
+
+    return () => {}
+  },[])
   return (
-    <div>ManageUsers</div>
+    <DashboardLayout activeMenu={"Team Members"}>ManageUsers</DashboardLayout>
   )
 }
 
